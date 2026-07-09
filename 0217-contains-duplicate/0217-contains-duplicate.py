1db0a7213402f0721fb730ff4563eb323c816a0f
Hoyoung Chung / 정호영 <235833667+cxo-ca@users.noreply.github.com>
2026-07-09 17:43:09 +0900

    Time: 18 ms (30.05%), Space: 32.4 MB (21.68%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -2,4 +2,11 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return(len(nums)) != len(set(nums))
+        seen = set()    # 지금까지 나온 수들을 저장할 집합
+
+        for num in nums:
+            if num in seen: # 이미 나온 숫자가 또 나올 시
+                return True # 중복이 있으므로 True 반환
+            seen.add(num)   # 처음 나온 숫자는 seen에 저장
+
+        return False    # 중복이 나오지 않으면 False 반환
